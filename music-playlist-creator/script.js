e3c686ce9b28f2211855300d0299385b5191db75
M H <mh@Ms-MacBook-Pro.local>
2024-05-10 12:43:57 -0700

    Styled playlist cards and modal with song cards

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,18 @@
 
+function createPlaylistCards(playlists) {
+
+}
+
+function createSongCard(song) {
+    /**
+     *  {
+          "songID": 0,
+          "title": "Nightcall",
+          "artist": "Kavinsky",
+          "album": "Nightcall",
+          "cover_art": "https://picsum.photos/id/43/200",
+          "duration": "4:18"
+        },
+     */
+    
+}
