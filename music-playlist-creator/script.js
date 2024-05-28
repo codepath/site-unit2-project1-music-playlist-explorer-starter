062a5a6cab368ab94debecbe6c11b088f73f9f50
ohziiilov3 <klbaskerville0520@gmail.com>
2024-05-28 16:32:23 -0700

    chore: updated modal overlay to cover top half of card and updated click event to open on overlay

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,25 +1,24 @@
 
 // JavaScript for Opening and Closing the Modal
 
-const modal = document.getElementById('playlistModal');
-const closeButton = document.querySelector('.close');
+const modal = document.getElementById("playlistModal");
+const closeButton = document.querySelector(".close");
 
-const cards = document.querySelectorAll('.card');
+const overlays = document.querySelectorAll(".card-overlay");
 
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-    
-        modal.style.display = 'block';
-    });
+overlays.forEach((overlay) => {
+  overlay.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
 });
 
-closeButton.addEventListener('click', () => {
-    // Hide the modal when close button is clicked
-    modal.style.display = 'none';
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
 });
 
-modal.addEventListener('click', event => {
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
 });
+
